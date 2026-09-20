13a67ff4ec32cc4e24db3760fe71d094b10a680a
Volcomix <Volcomix@users.noreply.github.com>
2026-09-20 10:54:12 +0200

    Bootstrap completion with gemma4

--- lua/codassist/init.lua
+++ lua/codassist/init.lua
@@ -14,6 +14,28 @@ function M.setup()
 		local suffix_lines = vim.api.nvim_buf_get_text(0, row, col, line_count, -1, {})
 		local suffix = table.concat(suffix_lines, "\n")
 		print("suffix ---\n" .. suffix .. "|||")
+
+		local body = vim.json.encode({
+			model = "gemma4",
+			prompt = "<|fim_prefix|>" .. prefix .. "<|fim_suffix|>" .. suffix .. "<|fim_middle|>",
+			stream = false,
+			think = false,
+			keep_alive = "60m",
+			options = {
+				temperature = 1.0,
+				top_p = 0.95,
+				top_k = 64,
+				num_predict = 128,
+			},
+		})
+
+		vim.system({ "curl", "http://localhost:11434/api/generate", "-d", body }, function(out)
+			local response = vim.json.decode(out.stdout).response
+			print("response ---\n" .. response .. "|||")
+			vim.schedule(function()
+				vim.api.nvim_put(vim.split(response, "\n"), "", false, true)
+			end)
+		end)
 	end)
 end
 
